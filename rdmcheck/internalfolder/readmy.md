6c33cf185ddd4bbf9fe7fb463c0d34db988ea7ce
Mykola Omelianov <mykola.omelyanov@gmail.com>
2020-01-23 12:19:14 +0200

    initial

---
+++ rdmcheck/internalfolder/readmy.md
@@ -0,0 +1,4 @@
+# markdown
+* test
+  check visibility
+  #### size test
---
+++ rdmcheck/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  rdmcheck
+//
+//  Created by Nikolas Omelianov on 23.01.2020.
+//  Copyright © 2020 Nikolas Omelianov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
